8038dca17d4152b8bc843e2499d3056021ce8f7f
Chunyang Guo <inevermore@foxmail.com>
2015-01-27 22:31:31 +0800

    注释单例模式类模板

--- muduo/base/Singleton.h
+++ muduo/base/Singleton.h
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include <stdlib.h> // atexit
 
+// 这个版本的单例模式，利用pthread_once实现了对象的唯一性
 namespace muduo
 {
 
@@ -20,11 +21,19 @@ namespace detail
 template<typename T>
 struct has_no_destroy
 {
+  // typeof 是GCC提供的关键字
   template <typename C> static char test(typeof(&C::no_destroy)); // or decltype in C++11
   template <typename C> static int32_t test(...);
   const static bool value = sizeof(test<T>(0)) == 1;
 };
 }
+// 对上面的has_no_destroy的解释如下：
+// 对于detail::has_no_destroy<T>::value
+// 如果是class类型，那么模板推断决议，将test<T>(0)推断至第一个版本，sizeof(test<T>(0))为1
+// value为true
+// 如果是int、double等类型，推断至第二个版本，value为False
+// 所以对于class类型，detail::has_no_destroy<T>::value测试为True
+// 对于int则测试为False
 
 template<typename T>
 class Singleton : boost::noncopyable
@@ -43,6 +52,7 @@ class Singleton : boost::noncopyable
   static void init()
   {
     value_ = new T();
+    //参见上面的解释，对于普通类型不注册析构函数
     if (!detail::has_no_destroy<T>::value)
     {
       ::atexit(destroy);
@@ -51,6 +61,7 @@ class Singleton : boost::noncopyable
 
   static void destroy()
   {
+    // 检测T的类型是否完整
     typedef char T_must_be_complete_type[sizeof(T) == 0 ? -1 : 1];
     T_must_be_complete_type dummy; (void) dummy;
 
