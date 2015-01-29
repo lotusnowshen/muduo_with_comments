70369ad2dd28027c52337908df42aa6dbdebd336
Chunyang Guo <inevermore@foxmail.com>
2015-01-29 22:12:44 +0800

    增加对Channel的注释

--- muduo/net/Channel.cc
+++ muduo/net/Channel.cc
@@ -17,9 +17,12 @@
 using namespace muduo;
 using namespace muduo::net;
 
-const int Channel::kNoneEvent = 0;
-const int Channel::kReadEvent = POLLIN | POLLPRI;
-const int Channel::kWriteEvent = POLLOUT;
+// Poll与EPoll中的事件相同，
+// POLLIN与EPOLLIN POLLPRI与EPOLLPRI POLLOUT与EPOLLOUT等价
+// 这里可以加上静态断言来确定
+const int Channel::kNoneEvent = 0; // 没有需要监听的事件
+const int Channel::kReadEvent = POLLIN | POLLPRI; // 读事件
+const int Channel::kWriteEvent = POLLOUT; // 写事件
 
 Channel::Channel(EventLoop* loop, int fd__)
   : loop_(loop),
@@ -34,6 +37,7 @@ Channel::Channel(EventLoop* loop, int fd__)
 {
 }
 
+// 析构时，必须确保loop中已经移除了该fd，或者epoll中已经删除了该fd
 Channel::~Channel()
 {
   assert(!eventHandling_);
@@ -50,12 +54,16 @@ void Channel::tie(const boost::shared_ptr<void>& obj)
   tied_ = true;
 }
 
+// 通过调用loop中函数，改变本fd在epoll中监听的事件
+// 在loop中又会去调用Poller中的函数，来更改epoll对fd监听的事件
 void Channel::update()
 {
   addedToLoop_ = true;
   loop_->updateChannel(this);
 }
 
+// 同上，通过EventLoop调用Epoller中函数，达到从epoll或者poll中删除fd的目的
+// 进行该操作时，必须确保该Channel已经不再监听任何事件
 void Channel::remove()
 {
   assert(isNoneEvent());
@@ -80,6 +88,7 @@ void Channel::handleEvent(Timestamp receiveTime)
   }
 }
 
+// 处理各种回调事件
 void Channel::handleEventWithGuard(Timestamp receiveTime)
 {
   eventHandling_ = true;
@@ -93,20 +102,20 @@ void Channel::handleEventWithGuard(Timestamp receiveTime)
     if (closeCallback_) closeCallback_();
   }
 
-  if (revents_ & POLLNVAL)
+  if (revents_ & POLLNVAL) // fd值非法，可能是fd已经关闭
   {
     LOG_WARN << "Channel::handle_event() POLLNVAL";
   }
 
-  if (revents_ & (POLLERR | POLLNVAL))
+  if (revents_ & (POLLERR | POLLNVAL)) // 错误事件处理
   {
     if (errorCallback_) errorCallback_();
   }
-  if (revents_ & (POLLIN | POLLPRI | POLLRDHUP))
+  if (revents_ & (POLLIN | POLLPRI | POLLRDHUP)) // 有数据可读
   {
     if (readCallback_) readCallback_(receiveTime);
   }
-  if (revents_ & POLLOUT)
+  if (revents_ & POLLOUT) // 该fd可写
   {
     if (writeCallback_) writeCallback_();
   }
--- muduo/net/Channel.h
+++ muduo/net/Channel.h
@@ -31,16 +31,25 @@ class EventLoop;
 /// This class doesn't own the file descriptor.
 /// The file descriptor could be a socket,
 /// an eventfd, a timerfd, or a signalfd
+
+// Channel对应一个文件描述符fd
+// Channel封装了一系列该fd对应的操作，使用回调函数的手法，
+// 包括可读、可写、关闭和错误处理四个回调函数
+// fd一般是tcp连接，也可以是其他，例如timerfd，甚至是文件fd
+
 class Channel : boost::noncopyable
 {
  public:
-  typedef boost::function<void()> EventCallback;
-  typedef boost::function<void(Timestamp)> ReadEventCallback;
+  typedef boost::function<void()> EventCallback; // 事件回调函数
+  typedef boost::function<void(Timestamp)> ReadEventCallback; // 读操作回调函数，需要传入时间
 
   Channel(EventLoop* loop, int fd);
   ~Channel();
 
+  // 处理回调事件，一般由poller通过eventLoop来调用
   void handleEvent(Timestamp receiveTime);
+
+  // 设置四种回调函数
   void setReadCallback(const ReadEventCallback& cb)
   { readCallback_ = cb; }
   void setWriteCallback(const EventCallback& cb)
@@ -49,6 +58,8 @@ class Channel : boost::noncopyable
   { closeCallback_ = cb; }
   void setErrorCallback(const EventCallback& cb)
   { errorCallback_ = cb; }
+
+  // 设置回调函数的C++11版本，使用了右值语义
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
   void setReadCallback(ReadEventCallback&& cb)
   { readCallback_ = std::move(cb); }
@@ -64,20 +75,22 @@ class Channel : boost::noncopyable
   /// prevent the owner object being destroyed in handleEvent.
   void tie(const boost::shared_ptr<void>&);
 
-  int fd() const { return fd_; }
-  int events() const { return events_; }
-  void set_revents(int revt) { revents_ = revt; } // used by pollers
+  int fd() const { return fd_; } // 返回该Channel对应的fd
+  int events() const { return events_; } // 返回该Channel正在监听的事件
+  void set_revents(int revt) { revents_ = revt; } // 进行poll或者epoll_wait调用后，根据fd的返回事件调用此函数
   // int revents() const { return revents_; }
-  bool isNoneEvent() const { return events_ == kNoneEvent; }
+  bool isNoneEvent() const { return events_ == kNoneEvent; } // 判断Channel是否没有事件处理
 
-  void enableReading() { events_ |= kReadEvent; update(); }
-  void disableReading() { events_ &= ~kReadEvent; update(); }
-  void enableWriting() { events_ |= kWriteEvent; update(); }
-  void disableWriting() { events_ &= ~kWriteEvent; update(); }
-  void disableAll() { events_ = kNoneEvent; update(); }
-  bool isWriting() const { return events_ & kWriteEvent; }
+  // update主要是通过eventloop去更新epoll中fd的监听事件
+  void enableReading() { events_ |= kReadEvent; update(); }  // 开始监听读事件
+  void disableReading() { events_ &= ~kReadEvent; update(); } // 停止监听读事件
+  void enableWriting() { events_ |= kWriteEvent; update(); } // 开始监听写事件 
+  void disableWriting() { events_ &= ~kWriteEvent; update(); } // 停止监听写事件
+  void disableAll() { events_ = kNoneEvent; update(); } // 停止监听所有事件
+  bool isWriting() const { return events_ & kWriteEvent; } // Channel是否在监听写事件，
+  // 因为poller只在有数据可写时，才去监听write事件，所以该函数的实际函数是Channel是否在执行写操作
 
-  // for Poller
+  // 下面函数主要是提供给poller使用
   int index() { return index_; }
   void set_index(int idx) { index_ = idx; }
 
@@ -86,28 +99,32 @@ class Channel : boost::noncopyable
 
   void doNotLogHup() { logHup_ = false; }
 
+  // 返回持有本Channel的EventLoop指针
   EventLoop* ownerLoop() { return loop_; }
+  // 将Channel从EventLoop中移除，其实也要从poller中停止监听该fd
   void remove();
 
  private:
-  void update();
-  void handleEventWithGuard(Timestamp receiveTime);
+  void update(); // 通过调用loop中函数，改变本fd在epoll中监听的事件
+  void handleEventWithGuard(Timestamp receiveTime); // 在临界区代码中处理事件
 
   static const int kNoneEvent;
   static const int kReadEvent;
   static const int kWriteEvent;
 
-  EventLoop* loop_;
-  const int  fd_;
-  int        events_;
-  int        revents_; // it's the received event types of epoll or poll
+  EventLoop* loop_; // 持有该Channel的EventLoop的指针
+  const int  fd_; // Channel对应的fd
+  int        events_; // 该fd正在监听的事件
+  int        revents_; // poll调用后，该fd需要处理的事件，依据它，poller调用它相应的回调函数
   int        index_; // used by Poller.
   bool       logHup_;
 
   boost::weak_ptr<void> tie_;
   bool tied_;
-  bool eventHandling_;
+  bool eventHandling_; // 是否正在处理事件
   bool addedToLoop_;
+
+  // 四种回调函数，使用的是boost提供的function模板
   ReadEventCallback readCallback_;
   EventCallback writeCallback_;
   EventCallback closeCallback_;
