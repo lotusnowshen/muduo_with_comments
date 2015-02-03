618b9502850fef342292e8b51debe22911e2e6ab
Chunyang Guo <inevermore@foxmail.com>
2015-02-03 22:17:14 +0800

    注释了EventLoopThread

--- muduo/net/EventLoopThread.cc
+++ muduo/net/EventLoopThread.cc
@@ -26,6 +26,11 @@ EventLoopThread::EventLoopThread(const ThreadInitCallback& cb)
 {
 }
 
+// 此EventLoop的退出流程：
+// 1.EventLoopThread对象过期
+// 2.如果loop_不为NULL，说明正在执行（因为loop初始为NULL，一旦运行起来，就再也不是NULL）
+// 需要将内部的EventLoop关闭
+// 3.join线程
 EventLoopThread::~EventLoopThread()
 {
   exiting_ = true;
@@ -38,14 +43,19 @@ EventLoopThread::~EventLoopThread()
   }
 }
 
+// 开启线程，并且返回持有的EventLoop的指针
 EventLoop* EventLoopThread::startLoop()
 {
   assert(!thread_.started());
   thread_.start();
 
+  // 这里存在一个race Condition
+  // 线程刚刚启动时，里面的EventLoop的指针还没有赋给loop_，所以
+  // 这里需要等待，当指针赋值成功后，cond_会通知这里继续执行
+
   {
     MutexLockGuard lock(mutex_);
-    while (loop_ == NULL)
+    while (loop_ == NULL) // 这里必须使用while循环等待
     {
       cond_.wait();
     }
@@ -54,15 +64,21 @@ EventLoop* EventLoopThread::startLoop()
   return loop_;
 }
 
+// 开启的新线程的执行逻辑
 void EventLoopThread::threadFunc()
 {
+  // 因为EventLoopThread运行在一个线程中，而
+  // 本函数又代表了线程的全部执行逻辑，所以这里将loop作为
+  // 一个栈对象，线程执行结束后，会自动销毁
   EventLoop loop;
 
+  // 执行线程的初始化回调函数，主要与loop有关
   if (callback_)
   {
     callback_(&loop);
   }
 
+  // 这里主要是消除race condition 防止上面的startLoop返回一个空指针
   {
     MutexLockGuard lock(mutex_);
     loop_ = &loop;
--- muduo/net/EventLoopThread.h
+++ muduo/net/EventLoopThread.h
@@ -22,6 +22,11 @@ namespace muduo
 namespace net
 {
 
+// 之前的EventLoop都是占据了一个线程，但是有些情况下，我们需要使用
+// 一个单独的线程去执行EventLoop，例如TCP的client，我们可能需要在
+// 当前线程中读取stdin，所以希望EventLoop在一个新的线程中运行
+// 这就是EventLoopThread产生的必要性
+
 class EventLoop;
 
 class EventLoopThread : boost::noncopyable
@@ -34,14 +39,22 @@ class EventLoopThread : boost::noncopyable
   EventLoop* startLoop();
 
  private:
-  void threadFunc();
+  void threadFunc(); // 线程的回调函数
 
-  EventLoop* loop_;
-  bool exiting_;
-  Thread thread_;
-  MutexLock mutex_;
-  Condition cond_;
-  ThreadInitCallback callback_;
+  // 这个类存在一个race condition，主要是loop初始化一个NULL
+  // 当开启loop后才将指针赋给它，所以直接执行startLoop可能
+  // 会在Loop开启后但还没有赋值给loop_之前返回，结果外界得到一个空指针，
+  // 所以这里需要一个Mutex，实现互斥，一个Condition用于实现线程内给loop_
+  // 赋值后，通知startLoop可以返回（因为startLoop可能在等待），以此实现startLoop
+  // 返回结果的正确性
+
+
+  EventLoop* loop_; // 本对象拥有的EventLoop的指针
+  bool exiting_; // 
+  Thread thread_; // 线程，在内部执行EventLoop
+  MutexLock mutex_; // 保护对loop_的互斥操作
+  Condition cond_; // 通知startLoop可以返回
+  ThreadInitCallback callback_; // 线程初始时的回调函数，只执行一次
 };
 
 }
