202cda9238aaa2ec1046e50942fc9487d566744f
Chunyang Guo <inevermore@foxmail.com>
2015-02-04 21:25:09 +0800

    注释EventLoopThreadPool

--- muduo/net/EventLoopThreadPool.cc
+++ muduo/net/EventLoopThreadPool.cc
@@ -18,56 +18,79 @@ using namespace muduo::net;
 
 
 EventLoopThreadPool::EventLoopThreadPool(EventLoop* baseLoop)
-  : baseLoop_(baseLoop),
+  : baseLoop_(baseLoop), //从外界接收一个loop
     started_(false),
-    numThreads_(0),
-    next_(0)
+    numThreads_(0), // 默认没有线程
+    next_(0) // loop调度专用，初始化为0
 {
 }
 
 EventLoopThreadPool::~EventLoopThreadPool()
 {
   // Don't delete loop, it's stack variable
+  // 这里无需delete loop，因为他们都是栈上的对象，
+  // 从EventLoopThread的源码中可以看到，loop是一个局部对象，
+  // 无须手工delete。
+  // 注意这个线程池如果析构：
+  // 1.成员对象threads_过期，需要析构
+  // 2.threads_析构导致内部的每个EventLoopThread被析构
+  // 3.每个线程内的EventLoop被执行loop
+  // 4.每个线程被join
+  // 5.所有的EventLoop以及所在线程全部销毁
+  // 6.本线程池不需要做任何任务
 }
 
+// 启动线程池
 void EventLoopThreadPool::start(const ThreadInitCallback& cb)
 {
   assert(!started_);
+  // 禁止跨线程，必须在baseLoop所在线程内开启线程池
   baseLoop_->assertInLoopThread();
 
   started_ = true;
 
   for (int i = 0; i < numThreads_; ++i)
   {
+    // 逐个创建线程
     EventLoopThread* t = new EventLoopThread(cb);
     threads_.push_back(t);
+    // 开启线程，开启EventLoop
     loops_.push_back(t->startLoop());
   }
+  // 如果线程数目为0，那么执行回调函数
   if (numThreads_ == 0 && cb)
   {
     cb(baseLoop_);
   }
 }
 
+// 这是一个调度算法，用于master Loop向线程池内的Loop分派任务
+// 如果无loop可以调度，那么只好向baseLoop，也就是向自身添加任务
 EventLoop* EventLoopThreadPool::getNextLoop()
 {
+  // 禁止跨线程，必须在baseLoop所在的线程中
   baseLoop_->assertInLoopThread();
   assert(started_);
   EventLoop* loop = baseLoop_;
 
-  if (!loops_.empty())
+  // 如果线程池内存在EventLoop
+  if (!loops_.empty()) 
   {
     // round-robin
+    // 这里使用轮转的算法调度EventLoop
     loop = loops_[next_];
     ++next_;
+    // 超过loop的数量就变为0，从头开始轮转
     if (implicit_cast<size_t>(next_) >= loops_.size())
     {
       next_ = 0;
     }
   }
-  return loop;
+  return loop; // 返回被调度的loop，注意这里如果loops为空，那么返回的是baseLoop
 }
 
+// 这也是一个调度算法，只是这里采用的不是轮转，而是hash
+// hash算法可以由外部执行
 EventLoop* EventLoopThreadPool::getLoopForHash(size_t hashCode)
 {
   baseLoop_->assertInLoopThread();
@@ -77,9 +100,10 @@ EventLoop* EventLoopThreadPool::getLoopForHash(size_t hashCode)
   {
     loop = loops_[hashCode % loops_.size()];
   }
-  return loop;
+  return loop; // 可能返回baseLoop自身
 }
 
+// 如果线程池内存在loop，则讲他们全部返回，否则返回baseLoop
 std::vector<EventLoop*> EventLoopThreadPool::getAllLoops()
 {
   baseLoop_->assertInLoopThread();
--- muduo/net/EventLoopThreadPool.h
+++ muduo/net/EventLoopThreadPool.h
@@ -25,6 +25,12 @@ namespace net
 class EventLoop;
 class EventLoopThread;
 
+// 前面的EventLoop和EventLoopThread都是单线程Reactor，区别仅仅是在当前
+// 线程内执行loop还是新开一个线程而已。
+// 这里的EventLoopThreadPool则是新开一个线程池，池内的每个Thread均执行一个
+// EventLoop，另外EventLoopThreadPool从外面接收一个loop作为master
+// 它与pool内的EventLoop是一个主从关系
+
 class EventLoopThreadPool : boost::noncopyable
 {
  public:
@@ -32,9 +38,11 @@ class EventLoopThreadPool : boost::noncopyable
 
   EventLoopThreadPool(EventLoop* baseLoop);
   ~EventLoopThreadPool();
+  // 设置线程数目 如果不设置，则为0
   void setThreadNum(int numThreads) { numThreads_ = numThreads; }
   void start(const ThreadInitCallback& cb = ThreadInitCallback());
 
+  // 下面是两个调度算法，必须在线程池start后使用
   // valid after calling start()
   /// round-robin
   EventLoop* getNextLoop();
@@ -42,19 +50,20 @@ class EventLoopThreadPool : boost::noncopyable
   /// with the same hash code, it will always return the same EventLoop
   EventLoop* getLoopForHash(size_t hashCode);
 
-  std::vector<EventLoop*> getAllLoops();
+  std::vector<EventLoop*> getAllLoops(); // 获取线程池所有的EventLoop
 
+  // 判断线程池是否开启
   bool started() const
   { return started_; }
 
  private:
 
-  EventLoop* baseLoop_;
-  bool started_;
-  int numThreads_;
-  int next_;
-  boost::ptr_vector<EventLoopThread> threads_;
-  std::vector<EventLoop*> loops_;
+  EventLoop* baseLoop_; // master Reactor线程 需要构造时从外部接收
+  bool started_;  // 线程池是否开启
+  int numThreads_; // 线程数目，也就是EventLoop的数目
+  int next_; // 用于轮转法中调度
+  boost::ptr_vector<EventLoopThread> threads_; //线程指针数组
+  std::vector<EventLoop*> loops_;  // 线程池内EventLoop的指针集合
 };
 
 }
