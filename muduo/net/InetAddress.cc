bb2cfb2b7bc61e70e57d2fa0bd118e8c1bed0f05
Chunyang Guo <inevermore@foxmail.com>
2015-02-04 21:50:28 +0800

    注释InetAddress

--- muduo/net/InetAddress.cc
+++ muduo/net/InetAddress.cc
@@ -20,7 +20,9 @@
 
 // INADDR_ANY use (type)value casting.
 #pragma GCC diagnostic ignored "-Wold-style-cast"
+// INADDR_ANY表示可以绑定任何一块网卡
 static const in_addr_t kInaddrAny = INADDR_ANY;
+// INADDR_LOOPBACK表示本地环路测试
 static const in_addr_t kInaddrLoopback = INADDR_LOOPBACK;
 #pragma GCC diagnostic error "-Wold-style-cast"
 
@@ -40,12 +42,14 @@ static const in_addr_t kInaddrLoopback = INADDR_LOOPBACK;
 using namespace muduo;
 using namespace muduo::net;
 
+// 使用静态断言，保证addr和InetAddress的size相同，保证兼容性
 BOOST_STATIC_ASSERT(sizeof(InetAddress) == sizeof(struct sockaddr_in));
 
 InetAddress::InetAddress(uint16_t port, bool loopbackOnly)
 {
   bzero(&addr_, sizeof addr_);
   addr_.sin_family = AF_INET;
+  // 根据loopbackOnly决定是采用INADDR_ANY还是INADDR_LOOPBACK
   in_addr_t ip = loopbackOnly ? kInaddrLoopback : kInaddrAny;
   addr_.sin_addr.s_addr = sockets::hostToNetwork32(ip);
   addr_.sin_port = sockets::hostToNetwork16(port);
@@ -76,8 +80,11 @@ uint16_t InetAddress::toPort() const
   return sockets::networkToHost16(addr_.sin_port);
 }
 
+// __thread代表线程内全局变量，多个线程访问不会相互干扰，
+// 这是gcc提供的功能，它使得下面的resolve变成一个线程安全的函数
 static __thread char t_resolveBuffer[64 * 1024];
 
+// DNS解析，输入一个主机名，解析为ip地址，将结果保存在out中
 bool InetAddress::resolve(StringArg hostname, InetAddress* out)
 {
   assert(out != NULL);
@@ -86,6 +93,12 @@ bool InetAddress::resolve(StringArg hostname, InetAddress* out)
   int herrno = 0;
   bzero(&hent, sizeof(hent));
 
+  // gethostbyname_r是一个可重入的、线程安全的函数，相比之下，gethostbyname使用的是
+  // static局部变量存储结果，使用看似简单，实际上丧失了可重入的能力
+  // struct hostent *gethostbyname(const char *name);
+  // 而gethostbyname_r采用外部传入存储空间，由用户自己保证存储空间的线程安全性即可
+  // 当然，如果将本函数用于信号处理函数handler中，导致同一函数对本函数调用两次或以上
+  // 仍然会导致结果错误
   int ret = gethostbyname_r(hostname.c_str(), &hent, t_resolveBuffer, sizeof t_resolveBuffer, &he, &herrno);
   if (ret == 0 && he != NULL)
   {
--- muduo/net/InetAddress.h
+++ muduo/net/InetAddress.h
@@ -25,15 +25,23 @@ namespace net
 /// Wrapper of sockaddr_in.
 ///
 /// This is an POD interface class.
+
+// 这个类是对socket中struct sockaddr_in做了一个简单地包装
+// 这是一个POD数据类型，具有值语义
+
 class InetAddress : public muduo::copyable
 {
  public:
   /// Constructs an endpoint with given port number.
   /// Mostly used in TcpServer listening.
+  // 根据loopbackOnly决定是采用INADDR_ANY还是INADDR_LOOPBACK
   explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false);
 
   /// Constructs an endpoint with given ip and port.
   /// @c ip should be "1.2.3.4"
+  // 使用ip和port创造一个addr，这里的ip使用的是StringArg，这表示
+  // 他可以同时接受C风格char *和std::string两种风格的字符串
+  // 这里的ip是文字形式 例如"192.168.1.150"
   InetAddress(StringArg ip, uint16_t port);
 
   /// Constructs an endpoint with given struct @c sockaddr_in
@@ -48,10 +56,14 @@ class InetAddress : public muduo::copyable
 
   // default copy/assignment are Okay
 
+  // 返回内部addr的const引用
   const struct sockaddr_in& getSockAddrInet() const { return addr_; }
+  // 设置内部的addr
   void setSockAddrInet(const struct sockaddr_in& addr) { addr_ = addr; }
 
+  // 返回网络字节序的ip地址
   uint32_t ipNetEndian() const { return addr_.sin_addr.s_addr; }
+  // 返回网络字节序的port
   uint16_t portNetEndian() const { return addr_.sin_port; }
 
   // resolve hostname to IP address, not changing port or sin_family
